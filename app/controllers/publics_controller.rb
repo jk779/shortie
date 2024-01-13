d677389699665c99450dc4a9ebc3817a978ea18c
jk779 <1104528+jk779@users.noreply.github.com>
2024-01-13 11:30:32 +0100

    Added support for full-domain-matches

--- app/controllers/publics_controller.rb
+++ app/controllers/publics_controller.rb
@@ -6,10 +6,17 @@ require 'uri'
 class PublicsController < ApplicationController
   layout false
 
+  TLKMDEV_REGEX = /\A[a-zA-Z0-9]+\.tlkm\.dev\z/
+
   # GET short_name.example.com
   def show
-    @url = Url.find_by(short_url: request.subdomain)
-    return render plain: 'Not found 🌚🌚', status: :not_found unless @url
+    @url = if request.host.match?(TLKMDEV_REGEX)
+      Url.find_by(short_url: request.subdomain)
+    elsif (matching_whole_host = Url.find_by(short_url: request.host))
+      matching_whole_host
+    else
+      return render plain: 'Not found 🌚🌚', status: :not_found
+    end
 
     response = Net::HTTP.get_response(URI.parse(@url.original_url))
 
